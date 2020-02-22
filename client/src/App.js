868465e16d4dde132c39cd280c1c50711da6ac42
josepaniagua <jpanjr@gmail.com>
2020-02-21 17:16:52 -0800

    data is being received by componentdidmount

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       {/*<header className="App-header">*/}
+//       {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+//       {/*  <p>*/}
+//       {/*    Edit <code>src/App.js</code> and save to reload.*/}
+//       {/*  </p>*/}
+//       {/*  <a*/}
+//       {/*    className="App-link"*/}
+//       {/*    href="https://reactjs.org"*/}
+//       {/*    target="_blank"*/}
+//       {/*    rel="noopener noreferrer"*/}
+//       {/*  >*/}
+//       {/*    Learn React*/}
+//       {/*  </a>*/}
+//       {/*</header>*/}
+//       <h1>Women's World Cup</h1>
+//     </div>
+//   );
+// }
+//
+// export default App;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import axios from 'axios'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        players: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players\n')
+        .then(res => {
+            const players = res.data;
+            this.setState({players});
+            console.log(this.state.players);
+        })
+        .catch(err => {
+            console.log('Error: ', err)
+        })
+  }
+
+
+
+    render() {
+    return (
+      <div>
+        <h1>Women's World Cup</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+//
+// const PlayerCard = () => {
+//     return (
+//         < div >
+//
+//         < /div>
+// )
+//     ;
+// };
+//
+// export default PlayerCard;
