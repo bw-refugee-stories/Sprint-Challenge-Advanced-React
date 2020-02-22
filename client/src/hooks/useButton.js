b9630fa8d7718d9b7966ba1a361482197432d7f0
josepaniagua <jpanjr@gmail.com>
2020-02-21 18:49:11 -0800

    usebutton completed

--- client/src/hooks/useButton.js
+++ client/src/hooks/useButton.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-export const useButton = () => {
-    const [buttonText, setButtonText] = useState("Click Me");
+export const useButton = (string1) => {
+    const [buttonText, setButtonText] = useState("Click for searches");
 
     const handleClick = () => {
-        return setButtonText("I was clicked");
+        return setButtonText(string1)
     };
 
-    return <button onClick={handleClick}>{buttonText}</button>;
+    return [buttonText, handleClick];
 };
